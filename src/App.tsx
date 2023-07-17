7a43342e17bb63a48833afde86532f0d82abe364
Reese Hatfield <hatfield.69@wright.edu>
2023-07-17 16:22:19 -0400

    Project Refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,18 @@
 import * as React from 'react'
-import { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 //@ts-ignore
-import Log from "./Components/Log/Log.tsx";
+import Art from './Components/Art/Art.tsx';
 //@ts-ignore
-import CurrentDir from "./Components/CurrentDir/CurrentDir.tsx";
-//@ts-ignore
-import InputBox from "./Components/InputBox/InputBox.tsx";
+import Title from './Components/Title/Title.tsx';
 
 function App() {
-    const baseDir = 'reesehatfield.github.io/Portfolio';
-    const [currentDir, setCurrentDir] = useState<string>(baseDir);
-    const [commands, setCommands] = useState<string[]>(['ls']);
-    const [repos, setRepos] = useState<any[]>([]);
-    const [readme, setReadme] = useState<string | null>(null);
-
-    const fetchRepos = async () => {
-        try {
-            const response = await fetch('https://api.github.com/users/reesehatfield/repos');
-            const data = await response.json();
-            setRepos(data);
-        } catch (error) {
-            console.error('Failed to fetch repositories:', error);
-        }
-    };
-
-    const fetchReadme = async (repo: string) => {
-        try {
-            const response = await fetch(`https://api.github.com/repos/reesehatfield/${repo}/readme`);
-            const data = await response.json();
-            const readmeContent = atob(data.content);
-            setReadme(readmeContent);
-        } catch (error) {
-            console.error('Failed to fetch readme:', error);
-        }
-    };
-
-    useEffect(() => {
-        fetchRepos();
-    }, []);
-
-    const handleDirClick = (directory: string) => {
-        const newCommands = [...commands];
-        newCommands.push(`cd ${directory}`);
-        setCommands(newCommands);
-
-        fetchReadme(directory);
-    };
-
     return (
-        <div className="App">
-            <Log commands={commands} handleDirClick={handleDirClick} readme={readme} repos={repos} />
-            <CurrentDir currentDir={currentDir} />
-        </div>
-    );
+        <>
+            <Title></Title>
+            <Art></Art>
+        </>
+    )
 }
 
 export default App;
---
+++ src/Components/Art/Art.tsx
@@ -0,0 +1,11 @@
+// Art.tsx
+import React from 'react';
+import './Art.css';
+
+export default function Art() {
+    return (
+        <>
+        <h1>Art goes here</h1>
+        </>
+    );
+}
