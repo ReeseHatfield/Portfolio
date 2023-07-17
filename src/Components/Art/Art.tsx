248df287fdff7a4e27f56f35e469d89a1ab59892
Reese Hatfield <hatfield.69@wright.edu>
2023-07-17 19:13:48 -0400

    Modal support added

--- src/Components/Art/Art.tsx
+++ src/Components/Art/Art.tsx
@@ -1,14 +1,20 @@
 // Art.tsx
 import React from 'react';
-import Gallery from 'react-photo-gallery';
-//@ts-ignore
-import { photos } from './photos.ts';
 import './Art.css';
+
+const imageNames = [
+    'IMG_0346.jpeg',
+    'IMG_0347.jpeg',
+    'IMG_0378.jpeg',
+  // Add more image names...
+];
 
 export default function Art() {
     return (
         <div className="art-container">
-            <Gallery photos={photos} />
+            {imageNames.map(name => (
+                <img src={`/images/${name}`} alt="" key={name} />
+            ))}
         </div>
     );
 }
