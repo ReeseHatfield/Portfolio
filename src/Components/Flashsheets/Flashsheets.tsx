f857358a972f95d2bf176d5ed020edfbcd0af008
Reese Hatfield <hatfield.69@wright.edu>
2023-07-26 12:03:14 -0400

    Add 404 redirect

--- src/Components/Flashsheets/Flashsheets.tsx
+++ src/Components/Flashsheets/Flashsheets.tsx
@@ -11,14 +11,13 @@ import flash3 from '../../images/IMG_0382.jpeg';
 import flash4 from '../../images/IMG_0383.jpeg';
 import flash5 from '../../images/IMG_0384.jpeg';
 import flash6 from '../../images/IMG_0385.jpeg';
-import flash7 from '../../images/IMG_0530.png';
 // this should be done via SSR, 
 // but since its being hosted with github pages
 // SSR is not feasible
 
 
 const images = [flash1, flash2, flash3, flash4,
-    flash5, flash6, flash7
+    flash5, flash6
 ]; // Add the imported images to this array
 
 export default function Art() {
