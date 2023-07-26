22adab6fffdd8d6f916febc3f7ecbffc3ba375cc
Reese Hatfield <hatfield.69@wright.edu>
2023-07-26 11:45:33 -0400

    Links now open in new tab

--- src/Components/Title/Title.tsx
+++ src/Components/Title/Title.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import './Title.css';
 
 export default function Title() {
-    return (
-        <div className="title-container">
-            <h1>Novalee Prince</h1>
-            <h3><a href="https://www.tiktok.com/@eepyeden">TikTok</a></h3>
-            <h3><a href="https://www.facebook.com/people/Novalee-Renee-Tattoo/100094110674933/">Facebook</a></h3>
-            <h3><a href="https://www.instagram.com/princenovalee/?igshid=OGQ5ZDc2ODk2ZA%3D%3D">Instagram</a></h3>
-        </div>
-    )
+  return (
+    <div className="title-container">
+      <h1>Novalee Prince</h1>
+      <h3><a target="_blank" href="https://www.tiktok.com/@eepyeden" rel="noreferrer">TikTok</a></h3>
+      <h3><a target="_blank" href="https://www.facebook.com/people/Novalee-Renee-Tattoo/100094110674933/" rel="noreferrer">Facebook</a></h3>
+      <h3><a target="_blank" href="https://www.instagram.com/princenovalee/?igshid=OGQ5ZDc2ODk2ZA%3D%3D" rel="noreferrer">Instagram</a></h3>
+    </div>
+  )
 }
